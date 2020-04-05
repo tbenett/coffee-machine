de1246d52c39fd273d6baad0153d47ea2bf5bc4c
acassaigne <anthony.cassaigne@gmail.com>
2020-04-05 16:12:35 +0200

    refacto to one class

--- src/main/java/Instruction.java
+++ src/main/java/Instruction.java
@@ -9,50 +9,13 @@ class Instruction {
   public static final String WITHOUT_STICK = "";
   public static final String MESSAGE_CODE = "M";
 
-  static String make(String order) {
 
-    if (isDrinkOrder(order)) {
-      return
-        makeDrinkCode(order) + SEPARATOR_CODE
-          + makeSugarQuantityCode(order) + SEPARATOR_CODE
-          + makeStickCode(order);
-    }
-
-    return makeMessage(order);
-  }
-
-  static String makeMessage(String message) {
-    return MESSAGE_CODE + SEPARATOR_CODE + message;
- }
-
-  private static boolean isDrinkOrder(String order) {
+  private boolean isDrinkOrder(String order) {
     return order.startsWith("tea") || order.startsWith("chocolate") || order.startsWith("coffee");
   }
 
-  static String makeSugarQuantityCode(String drinkOrder) {
-    return hasSugar(drinkOrder) ? extractQuantitySugar(drinkOrder) : WITHOUT_SUGAR_CODE;
-  }
 
-  static String makeStickCode(String drinkOrder) {
-    return hasSugar(drinkOrder) ? WITH_STICK : WITHOUT_STICK;
-  }
-
-  static String extractQuantitySugar(String drinkOrder) {
-    Pattern patternDrinkWithSugar = Pattern.compile(".*(1|2) sugar[s]?");
-    Matcher matcherDrinkWithSugar = patternDrinkWithSugar.matcher(drinkOrder);
-    matcherDrinkWithSugar.find();
-
-    return matcherDrinkWithSugar.group(1);
-  }
-
-  static boolean hasSugar(String drinkOrder) {
-    Pattern patternDrinkWithSugar = Pattern.compile(".*sugar[s]?");
-    Matcher matcherDrinkWithSugar = patternDrinkWithSugar.matcher(drinkOrder);
-
-    return matcherDrinkWithSugar.find();
-  }
-
-  static String makeDrinkCode(String drinkOrder) {
+  String makeDrinkCode(String drinkOrder) {
     if (drinkOrder.startsWith("tea")) {
       return "T";
     }
@@ -63,5 +26,44 @@ class Instruction {
       return "H";
     }
     return "";
+  }
+
+  String makeStickCode(String drinkOrder) {
+    return new Instruction().hasSugar(drinkOrder) ? WITH_STICK : WITHOUT_STICK;
+  }
+
+  boolean hasSugar(String drinkOrder) {
+    Pattern patternDrinkWithSugar = Pattern.compile(".*sugar[s]?");
+    Matcher matcherDrinkWithSugar = patternDrinkWithSugar.matcher(drinkOrder);
+
+    return matcherDrinkWithSugar.find();
+  }
+
+  String extractQuantitySugar(String drinkOrder) {
+    Pattern patternDrinkWithSugar = Pattern.compile(".*(1|2) sugar[s]?");
+    Matcher matcherDrinkWithSugar = patternDrinkWithSugar.matcher(drinkOrder);
+    matcherDrinkWithSugar.find();
+
+    return matcherDrinkWithSugar.group(1);
+  }
+
+  String makeSugarQuantityCode(String drinkOrder) {
+    return hasSugar(drinkOrder) ? extractQuantitySugar(drinkOrder) : WITHOUT_SUGAR_CODE;
+  }
+
+  String makeMessage(String message) {
+    return MESSAGE_CODE + SEPARATOR_CODE + message;
+ }
+
+  String make(String order) {
+
+    if (isDrinkOrder(order)) {
+      return
+        makeDrinkCode(order) + SEPARATOR_CODE
+          + makeSugarQuantityCode(order) + SEPARATOR_CODE
+          + makeStickCode(order);
+    }
+
+    return makeMessage(order);
   }
 }
