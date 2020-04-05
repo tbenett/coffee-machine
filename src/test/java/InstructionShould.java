c10b129125e01528a27c67f6b030e1d53b38afc0
acassaigne <anthony.cassaigne@gmail.com>
2020-04-05 16:12:48 +0200

    refacto to one class

--- src/test/java/InstructionShould.java
+++ src/test/java/InstructionShould.java
@@ -8,8 +8,8 @@ class InstructionShould {
 
   @ParameterizedTest
   @CsvSource({"coffee, C::" , "chocolate, H::", "tea, T::"})
-  void make_drink_instruction_without_sugar(String customerOrder, String instruction) {
-    assertThat(Instruction.make(customerOrder)).isEqualTo(instruction) ;
+  void make_drink_instruction_without_sugar(String drink, String instruction) {
+    assertThat(new Instruction().make(drink)).isEqualTo(instruction) ;
   }
 
   @ParameterizedTest
@@ -20,13 +20,13 @@ class InstructionShould {
     "chocolate with 1 sugar, H:1:0"
   })
   void make_drink_instruction_with_sugar_and_stick(String drink, String instruction) {
-    assertThat(Instruction.make(drink)).isEqualTo(instruction);
+    assertThat(new Instruction().make(drink)).isEqualTo(instruction);
   }
 
   @Test
   void make_an_instruction_message() {
     String message = "hello";
 
-    assertThat(Instruction.make(message)).isEqualTo("M:" + message);
+    assertThat(new Instruction().make(message)).isEqualTo("M:" + message);
   }
 }
