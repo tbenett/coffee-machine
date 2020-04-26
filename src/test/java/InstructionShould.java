3119f691de05c7cab805c2f5f32c9ed2028d1db4
tbenett <thomas@benett.io>
2020-04-26 15:45:16 +0200

    not enough money implemented

--- src/test/java/InstructionShould.java
+++ src/test/java/InstructionShould.java
@@ -43,29 +43,13 @@ class InstructionShould {
   // tea is 0.4
   // chocolate is 0.5
   // coffee is 0.6
-
-  @Test
-  void name() {
-    assertThat(foo( "tea with 1 sugar", 20)).startsWith("M:Not enough money");
-  }
-
-  @Test
-  void name_() {
-    assertThat(foo( "chocolate with 1 sugar", 41)).startsWith("M:Not enough money");
-  }
-
-  @Test
-  void name__() {
-    assertThat(foo( "coffee", 59)).startsWith("M:Not enough money");
-  }
-
   @ParameterizedTest
   @CsvSource({
           "tea with 1 sugar, 20",
           "chocolate with 1 sugar, 41",
           "coffee, 59",
   })
-  void name___(String drink, int cents) {
+  void give_a_message_not_enough_money(String drink, int cents) {
     assertThat(foo(drink, cents)).startsWith("M:Not enough money");
   }
 
@@ -74,32 +58,40 @@ class InstructionShould {
           "tea with 1 sugar, 10, M:Not enough money missing 30 cents",
           "tea with 1 sugar, 20, M:Not enough money missing 20 cents",
           "tea with 1 sugar, 30, M:Not enough money missing 10 cents",
+          "coffee with 1 sugar, 59, M:Not enough money missing 1 cents",
+          "chocolate, 41, M:Not enough money missing 9 cents",
   })
-  void give_a_message_not_enough_money(String drink, int cents, String instruction) {
+  void give_a_message_not_enough_money_with_missing_amount(String drink, int cents, String instruction) {
     assertThat(foo(drink, cents)).isEqualTo(instruction);
   }
 
   String foo(String order, int cents) {
-    if (InstructionFactory.isTea(order)) {
-      var missing = 40 - cents;
-      if (missing > 0) {
-        return InstructionFactory.create(String.format("Not enough money missing %d cents", missing)).toString();
-      }
-    }
-    if (InstructionFactory.isHotChocolate(order)) {
-      var missing = 50 - cents;
-      if (missing > 0) {
-        return InstructionFactory.create("Not enough money").toString();
-      }
-    }
+    final var missingAmount = missingAmount(order, cents);
 
-    if (InstructionFactory.isCoffee(order)) {
-      var missing = 60 - cents;
-      if (missing > 0) {
-        return InstructionFactory.create("Not enough money").toString();
-      }
+    if (missingAmount > 0) {
+      return InstructionFactory.create(String.format("Not enough money missing %d cents", missingAmount)).toString();
     }
 
     return InstructionFactory.create(order).toString();
+  }
+
+  private int missingAmount(String order, int cents) {
+    return minimalPriceForDrink(order) - cents;
+  }
+
+  private int minimalPriceForDrink(String order) {
+    if (InstructionFactory.isTea(order)) {
+      return 40;
+    }
+
+    if (InstructionFactory.isHotChocolate(order)) {
+      return 50;
+    }
+
+    if (InstructionFactory.isCoffee(order)) {
+      return 60;
+    }
+
+    return 0;
   }
 }
