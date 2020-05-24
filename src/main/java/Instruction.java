c78dc400608bf54e28d2e648415f7cfe4d68f4fc
acassaigne <anthony.cassaigne@gmail.com>
2020-05-24 15:04:38 +0200

    wip

--- src/main/java/Instruction.java
+++ src/main/java/Instruction.java
@@ -1,6 +1,12 @@
 abstract class Instruction {
   public static final String SEPARATOR_CODE = ":";
 
+   static public Instruction fromString(String instruction) {
+     if (instruction.startsWith("C")) {
+       
+     }
+   }
+
   abstract String make();
 
   public String toString() {
